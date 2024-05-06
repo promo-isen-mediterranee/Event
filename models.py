b054b35b6dfaac240723e04d707c57fb3c5c3870
Marc ETAVARD <rakushain@gmail.com>
2024-05-06 15:02:32 +0200

    Restructuration

---
+++ models.py
@@ -0,0 +1,179 @@
+from app import db
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql.expression import func, text
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(UUID, primary_key=True, unique=True, nullable=False, default=uuid.uuid4)
+    email = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def json(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+        }
+
+
+class Person(db.Model):
+    __tablename__ = "person"
+
+    id = db.Column(UUID, primary_key=True, nullable=False, default=uuid.uuid4)
+    last_name = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Person {self.first_name}>'
+
+    def json(self):
+        return {
+            'id': self.id,
+            'last_name': self.last_name,
+            'first_name': self.first_name
+        }
+    
+
+class Location(db.Model):
+    __tablename__ = "location"
+
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    room = db.Column(db.String(10), nullable=True)
+
+    def __repr__(self):
+        return f'<Location {self.city}>'
+
+    def json(self):
+        return {
+            'id': self.id,
+            'address': self.address,
+            'city': self.city,
+            'room': self.room,
+        }
+
+
+class Event_status(db.Model):
+    __tablename__ = "event_status"
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'<Event_status {self.label}>'
+
+    def json(self):
+        return {
+            'id': self.id,
+            'label': self.label,
+        }
+    
+
+class Event(db.Model):
+    __tablename__ = "event"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(265), nullable=False)
+    stand_size = db.Column(db.Integer, nullable=True)
+    contact_objective = db.Column(db.Integer, nullable=False, default = 100)
+    date_start = db.Column(db.DateTime, nullable=False)
+    date_end = db.Column(db.DateTime, nullable=False)
+    status_id = db.Column(db.Integer, db.ForeignKey('event_status.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    item_manager = db.Column(UUID, db.ForeignKey('person.id'))
+
+    r_stat = db.relationship(Event_status, backref="event_status", cascade="save-update")
+    r_loc = db.relationship(Location, backref="location", cascade="save-update")
+    r_item_manager = db.relationship(Person, backref="person", cascade="save-update")
+
+    def __repr__(self):
+        return f'<Event {self.name}>'
+
+    def json(self):
+        loc = Location.query.filter_by(id=self.location_id).first()
+        person = Person.query.filter_by(id=self.item_manager).first()
+        return {
+            'id': self.id,
+            'name': self.name,
+            'stand_size': self.stand_size,
+            'contact_objective': self.contact_objective,
+            'date_start': self.date_start.strftime('%Y-%m-%d'),
+            'date_end': self.date_end.strftime('%Y-%m-%d'),
+            'status': {"id": Event_status.query.filter_by(id=self.status_id).first().id,
+                       "label": Event_status.query.filter_by(id=self.status_id).first().label},
+            'item_manager': {'id': person.id,
+                             'name': person.last_name,
+                             'surname': person.first_name},
+            'location': {'id': loc.id,
+                         'address': loc.address,
+                         'city': loc.city,
+                         'room': loc.room}
+        }
+
+
+class Event_status_history(db.Model):
+    __tablename__ = "event_status_history"
+
+    id = db.Column(db.Integer, primary_key=True)
+    status_id = db.Column(db.Integer, db.ForeignKey('event_status.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    set_on = db.Column(db.DateTime(timezone=True))
+    set_by = db.Column(UUID, db.ForeignKey('users.id'))
+
+    r_event_history = db.relationship(Event, backref="event_history", cascade="save-update, delete")
+    r_stat_history = db.relationship(Event_status, backref="event_status_history", cascade="save-update")
+    r_user_history = db.relationship(Users, backref="users_history", cascade="save-update")
+
+    def __repr__(self):
+        return f'<Event_status_history {self.set_on}>'
+
+    def json(self):
+        return {
+            'id': self.id,
+            'status_id': self.status_id,
+            'event_id': self.event_id,
+            'set_on': self.set_on,
+            'set_by': self.set_by,
+        }
+
+def get_status_id(label):
+    status = Event_status.query.filter_by(label=label).first()
+    return status.id
+
+
+def get_manager_id(last_name, first_name):
+    manager = Person.query.filter_by(last_name=last_name, first_name=first_name).first()
+    if manager is None:
+        new_manager = Person(last_name=last_name, first_name=first_name)
+        db.session.add(new_manager)
+        db.session.commit()
+        return new_manager.id
+    else:
+        return manager.id
+
+
+def get_location_id(address, city, room):
+    loc = Location.query.filter_by(address=address, city=city, room=room).first()
+    if loc is None:
+        new_loc = Location(id=db.session.query(func.max(Location.id) + 1), address=address, city=city, room=room)
+        db.session.add(new_loc)
+        db.session.commit()
+        return new_loc.id
+    else:
+        return loc.id
+
+
+def change_history(event):
+    history = Event_status_history(status_id=event.status_id,
+                                   event_id=event.id,
+                                   set_on=func.now().op('AT TIME ZONE')(text("'Europe/Paris'")),
+                                   set_by=Users.query.filter_by(email="marc.etavard@isen.yncrea.fr").first().id
+                                   # TODO -> current user
+                                   )
+    db.session.add(history)
+    db.session.commit()
---
+++ routes.py
@@ -0,0 +1,182 @@
+from app import db, app
+from flask import request
+from models import Event, Event_status, Event_status_history, get_manager_id, change_history, get_location_id, get_status_id
+from werkzeug.exceptions import NotFound, BadRequest
+from sqlalchemy.sql.expression import func
+
+
+@app.route('/event')
+def get_event_data():
+    try:
+        event_list = Event.query.all()
+        return [event.json() for event in event_list], 200
+    except Exception as e:
+        return f'Error getting events, {e}', 500
+
+
+@app.route('/event/<int:id>/')
+def get_event(id):
+    event = Event.query.get_or_404(id)
+    return event.json()
+
+
+@app.route('/event/<string:event_status>')
+def get_events_by_status(event_status):
+    try:
+        status_id = Event_status.query.filter_by(label=event_status).first().id
+        event_list = Event.query.filter_by(status_id=status_id).all()
+        return [event.json() for event in event_list], 200
+    # except Unauthorized as e:
+    #     return make_response(jsonify({f'Utilisateur non authentifié, {e}'}), 401)
+    except NotFound as e:
+        return f'Aucun evenement trouvé, {e}', 404
+    except KeyError as e:
+        return f'Le statut fourni est incorrect, {e}', 400
+    except Exception as e:
+        return f'Error getting items location using item id, {e}', 500
+
+
+@app.route('/event/history/<int:event_id>/')
+def get_event_history(event_id):
+    try:
+        event = Event.query.get_or_404(event_id)
+        event_status_history = Event_status_history.query.filter_by(event_id=event_id).all()
+        return [event.json(), [history.json() for history in event_status_history]]
+    except NotFound as e:
+        return f'Aucun evenement trouvé, {e}', 404
+    except Exception as e:
+        return f'Error getting items location using item id, {e}', 500
+    
+
+
+@app.route('/event/create', methods=['POST'])
+def create_event():
+    try:
+        request_form = request.get_json()
+        name = request_form['name']
+        stand_size=0 if 'stand_size' not in request_form else int(request_form['stand_size'])
+        # valeur par défaut si contact_objective pas spécifié : 100
+        contact_objective=100 if 'contact_objective' not in request_form else int(request_form['contact_objective'])
+        date_start = request_form['date_start']
+        date_end = request_form['date_end']
+
+        if 'item_manager' in request_form:
+            item_manager = request_form['item_manager']
+            last_name = item_manager['last_name']
+            first_name = item_manager['first_name']
+        else:
+            last_name='A'
+            first_name='Definir'
+
+        if 'status' in request_form:
+            status = request_form['status']
+            label = status['label']
+        else:
+            label = 'A faire'
+
+        location = request_form['location']
+        address = location['address']
+        city = location['city']
+        room = '' if 'room' not in location else location['room']
+
+        item_manager = get_manager_id(last_name, first_name)
+        location_id = get_location_id(address, city, room)
+        status_id = get_status_id(label)
+
+
+        event = Event(id=db.session.query(func.max(Event.id) + 1),
+                    name=name,
+                    stand_size=stand_size,
+                    contact_objective=contact_objective,
+                    date_start=date_start,
+                    date_end=date_end,
+                    status_id= status_id,
+                    item_manager=item_manager,
+                    location_id=location_id)
+
+        db.session.add(event)
+        db.session.commit()
+
+        return 'Event created', 201
+
+    # except Unauthorized as e:
+    #     return ({f'Utilisateur non authentifié, {e}'}), 401)
+    # except Forbidden as e:
+        # return ({f'Droits non suffisants, {e}'}), 403)
+    # except KeyError:
+    #     return ({f'La requête est incomplète'}), 400)
+    except Exception as e:
+        return f'Erreur lors de la création de l evenement : {e}', 500
+
+
+@app.route('/event/<int:event_id>/', methods=['PUT'])
+def update_event(event_id):
+    try:
+        event = Event.query.get_or_404(event_id)
+        prev_status = Event_status.query.get_or_404(event.status_id)
+        if event:
+            request_form = request.get_json()
+
+            name = request_form['name']
+            date_start = request_form['date_start']
+            date_end = request_form['date_end']
+            event.name = name
+            event.date_start = date_start
+            event.date_end = date_end
+
+            if 'stand_size' in request_form:
+                event.stand_size = int(request_form['stand_size'])
+            if 'contact_objective' in request_form:
+                event.contact_objective = int(request_form['contact_objective'])
+            if 'status' in request_form:
+                status = request_form['status']
+                label = status['label']
+                new_status = Event_status.query.filter_by(label=label).first()
+                event.status_id = new_status.id
+                print(prev_status.label, label)
+                if prev_status.label != label:
+                    change_history(event)  # changement status -> event_status_history stocke le nouveau
+            
+            if 'item_manager' in request_form:
+                item_manager = request_form['item_manager']
+                last_name = item_manager['last_name']
+                first_name = item_manager['first_name']
+                event.item_manager = get_manager_id(last_name, first_name)
+            if 'location' in request_form:
+                location = request_form['location']
+                address = location['address']
+                city = location['city']
+                room = '' if 'room' not in location else location['room']
+                event.location_id = get_location_id(address, city, room)
+
+            db.session.commit()
+            return 'Evenement mis à jour', 201
+        
+    except NotFound as e:    
+        return 'Evenement à mettre à jour introuvable', 404
+    # except Unauthorized as e:
+    #     return ({f'Utilisateur non authentifié, {e}'}), 401)
+    except BadRequest as e:
+        return f'La requête est incomplète, {e} manquant', 400
+    except Exception as e:
+        return f'Erreur mise à jour de l évènement, {e} manquant', 500
+
+
+@app.route('/event/<int:event_id>/', methods=['DELETE'])
+def delete_event(event_id):
+    try:
+        event = Event.query.get_or_404(event_id)
+        if event:
+            db.session.delete(event)
+            db.session.commit()
+            return 'Evenement supprimé', 204
+
+    except NotFound as e:
+        return f'Evenement à supprimer introuvable, {e}', 404
+    # except Unauthorized as e:
+    #     return ({f'Utilisateur non authentifié, {e}'}), 401)    
+    except BadRequest as e:
+        return f'ID fourni incorrect, {e}', 400
+    except Exception as e:
+        return f'Error deleting event, {e}', 500
+
